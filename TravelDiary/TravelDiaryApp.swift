e4fb99eebeaa6df51ed08a4ad7609fd36cc3c8c0
Shreya Prasad <prasadshreya2303@gmail.com>
2025-08-08 13:22:16 +0530

    Initial Commit

---
+++ TravelDiary/TravelDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiaryApp.swift
+//  TravelDiary
+//
+//  Created by Shreya Prasad on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
